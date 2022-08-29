538c505218fdd77644a8d4c910c14b9b660af1a9
Karan Pandey <karpandey2002@gmail.com>
2022-08-29 17:59:21 +0530

    Added Main Page Layout to the screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import Navs from "./components/nav"
+import Home from "./pages/Home"
+import Starred from './pages/Starred';
 function App() {
   return (
     <BrowserRouter>
+    
       <Routes>
-        <Route path="/" element={"This is "} />
-        <Route path="/starred" element={"this is the starred"} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/starred" element={<Starred/>} />
         <Route element={"ooo"} />
 
       </Routes>
+      
     </BrowserRouter>
+    
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainPageLayout from "../components/MainPageLayout"
+const Home=()=>{
+    return (
+        <MainPageLayout>
+            this is home
+        </MainPageLayout>
+    )
+}
+export default Home
